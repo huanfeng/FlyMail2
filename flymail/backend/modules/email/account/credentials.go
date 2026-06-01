dc459aa0c553967a5afc21d4b9f76ed6c6995509
huanfeng <huanfengf@gmail.com>
2026-06-01 23:03:44 +0800

    feat(flymail): account.SMTPConfig（解密构建 SMTP 配置）

--- flymail/backend/modules/email/account/credentials.go
+++ flymail/backend/modules/email/account/credentials.go
@@ -39,6 +39,37 @@ func (s *Service) IMAPConfig(id uint) (types.IMAPConfig, error) {
 	}, nil
 }
 
+// SMTPConfig 取出账户并解密凭证，构建 core 的 SMTP 配置（供发送使用）。
+func (s *Service) SMTPConfig(id uint) (types.SMTPConfig, error) {
+	a, err := s.repo.GetByID(id)
+	if err != nil {
+		return types.SMTPConfig{}, err
+	}
+	pw, err := s.enc.Decrypt(a.PasswordEnc)
+	if err != nil {
+		return types.SMTPConfig{}, err
+	}
+	var proxy *types.ProxyConfig
+	if a.ProxyHost != "" {
+		ppw, err := s.enc.Decrypt(a.ProxyPasswordEnc)
+		if err != nil {
+			return types.SMTPConfig{}, err
+		}
+		proxy = &types.ProxyConfig{
+			Type: a.ProxyType, Host: a.ProxyHost, Port: a.ProxyPort,
+			Username: a.ProxyUsername, Password: ppw,
+		}
+	}
+	return types.SMTPConfig{
+		Host:     a.SMTPHost,
+		Port:     a.SMTPPort,
+		Username: a.LoginName(),
+		Password: pw,
+		Security: parseSecurity(a.SMTPSecurity),
+		Proxy:    proxy,
+	}, nil
+}
+
 // TouchLastSync 更新账户的最后同步时间。
 func (s *Service) TouchLastSync(id uint, t time.Time) error {
 	a, err := s.repo.GetByID(id)
